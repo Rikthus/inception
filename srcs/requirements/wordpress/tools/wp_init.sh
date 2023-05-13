e9b081cb9c3d67d8f249f9247ec4920b6b415b4b
maxperei <maxperei@student.42lyon.fr>
2023-05-13 23:47:57 +0200

    Update config files, project almost done

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -3,7 +3,7 @@
 cd /var/www/html/
 if ! wp core is-installed --allow-root
 then
-	wait 5
+	sleep 5
 	wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PWD} --dbhost=my-mariadb
 	wp core install --allow-root --url=maxperei.42.fr --title=INCEPTION_DONE --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=maxperei@student.42lyon.fr
 	wp user create --allow-root ${WP_USER} maxperei@student.42lyon.fr --user_pass=${WP_USER_PWD}
