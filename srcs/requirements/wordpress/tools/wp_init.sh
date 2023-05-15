a144ddf2948ff85282a02c755c8274be304d3713
lol <lol@debian>
2023-05-15 17:36:56 -0400

    Almost working, need to fix server address can be used with http

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-cd /var/www/html/
+cd /var/www/html/inception
 if ! wp core is-installed --allow-root
 then
 	sleep 5
 	wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PWD} --dbhost=mariadb
 	wp core install --allow-root --url=maxperei.42.fr --title=INCEPTION_DONE --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PWD} --admin_email=maxperei@student.42lyon.fr
-	wp user create --allow-root ${WP_USER} maxperei@student.42lyon.fr --user_pass=${WP_USER_PWD}
+	wp user create --allow-root ${WP_USER} xptdr@student.lyon.fr --user_pass=${WP_USER_PWD}
 fi
 
 mkdir -p /run/php/
